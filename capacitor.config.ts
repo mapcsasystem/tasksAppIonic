d3508b7328c2343db30da7fab66a7c97c9e61404
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-25 16:49:37 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'task-app-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
